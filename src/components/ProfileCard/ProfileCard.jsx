778552aba1b63ae2c6a840780bdaa060ddd7971b
zoe-woodward <zoewoodward94@gmail.com>
2024-11-17 17:58:13 +0000

    task 1

--- src/components/ProfileCard/ProfileCard.jsx
+++ src/components/ProfileCard/ProfileCard.jsx
@@ -1,27 +1,27 @@
 import './ProfileCard.css'
 
-export default function ProfileCard(props) {
-    return(
+
+export default function ProfileCard({ pic, name, description, email }) {
+    return (
         <div id="single-article-1" className="single-article">
-            <div id="card-front-1" className="front-card tb-card">
-                <img src={props.pic} className="profile-image" alt='' />
+             <div id="card-front-1" className="front-card tb-card">
+                <img src={pic} className="profile-image" alt={`${name}'s profile`} />
                 <div className="single-content">
                     <div className="card-middle">
-                        <h1>Name Here</h1>
-                        <p className="team-p1">
-                            Description here description here description here description here description here description here description here description here description here description here description here description here description here ... 
-                        </p>
+                        <h1>{name}</h1>
+                        <p className="team-p1">{description}</p>
                     </div>
                     <div className="card-bottom">
-                        <div className="card-email">
-                            insert@email.here
-                        </div>
-                        <div className="card-icon profile-trigger" data-id="single-article-1" >
+                        <div className="card-email">{email}</div>
+                        <div className="card-icon profile-trigger">
                             <i className="fa fa-chevron-right"></i>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    )
+    );
 }
+
+
+
